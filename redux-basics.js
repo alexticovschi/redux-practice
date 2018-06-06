82ccc4ef36d3359a1a7605e8e7b3e803d6965384
AlexTicovschi <alexmticovschi@gmail.com>
2018-06-06 13:11:57 +0100

    adding subscriptions

--- redux-basics.js
+++ redux-basics.js
@@ -26,9 +26,20 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+
+// Subscription
+// subscription gets triggered whenever an action is dispatched
+// it's get triggered whenever the state is updated
+// subscriptions inform us whenever we need to get a new state because something changed
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
+
+
 // Dispatch action
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
 
-// Subscription
+
+
