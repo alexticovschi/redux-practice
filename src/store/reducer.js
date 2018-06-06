85a2b69682b822c04d6f2fdbbc79111c5dc1cc2d
AlexTicovschi <alexmticovschi@gmail.com>
2018-06-06 13:20:40 +0100

    connecting React to Redux

---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    counter : 0
+}
+
+// reducer is just a function that retrieves the state and an action
+const reducer = (state = initialState, action) => {
+    return state;
+}
+
+export default reducer;
